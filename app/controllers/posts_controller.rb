f1d8905a93ba121bd7adffae20a575c04a163ff7
Abe Anderson <aberanderson@gmail.com>
2016-01-27 19:50:54 -0500

    finish assignment 31

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,16 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all 
+    @posts = Post.all
+
+    @posts.each_with_index do  |x, y|
+      #if (y+1) % 5 == 0 || y == 0 -- I misunderstood the directions and
+      #was kinda proud of the ever slightly more complex solution
+      #so I'm leaving it there
+      if y % 5 == 0
+        x.title = "SPAM"
+      end
+    end
+
   end
 
   def show
