394778acb3279694d0143c4c0ebb578f81add6a7
Abe Anderson <aberanderson@gmail.com>
2016-01-29 18:05:09 -0500

    finish everything but edit, update, and delete

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,37 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new
+    @question.title = params[:question][:title]
+    @question.body = params[:question][:body]
+    @question.resolved = params[:question][:resolved]
+
+    if @question.save
+      flash[:notice] = "Question saved."
+      redirect_to @question
+    else
+      flash.now[:alert] = "There was an error saving the question. Please try again."
+      render :new
+    end
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ spec/models/question_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+  let(:question) {Question.create!(title: "New Question", body: "New Body")}
+  describe "attributes" do
+    it "should respond to title" do
+      expect(question).to respond_to(:title)
+    end
+    it "should respond to body" do
+      expect(question).to respond_to(:title)
+    end
+  end
+end
