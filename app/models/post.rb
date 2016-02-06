c40712f7c309c9a34dda68617b7257d729949abe
Abe Anderson <aberanderson@gmail.com>
2016-02-06 15:50:07 -0500

    finish checkpoint43

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,7 @@ class Post < ActiveRecord::Base
   has_many :labels, through: :labelings
 
 
-  default_scope {order('created_at DESC')}
+  default_scope {order('rank DESC')}
 
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: {minimum:20}, presence: true
@@ -23,8 +23,14 @@ class Post < ActiveRecord::Base
   def down_votes
     votes.where(value: -1).count
   end
-  
+
   def points
     votes.sum(:value)
+  end
+
+  def update_rank
+    age_in_days = (created_at - Time.new(1970,1,1))/1.day.seconds
+    new_rank = points + age_in_days
+    update_attribute(:rank, new_rank)
   end
 end
