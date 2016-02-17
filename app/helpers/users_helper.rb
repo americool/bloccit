a36a0d643a0cd5a56a857e76bda4779d4cfd58d8
Abe Anderson <aberanderson@gmail.com>
2016-02-17 13:38:56 -0500

    almost finish part 1 (1 error) finish part 2 (not as clean as I'd like) part 3 need help

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,19 @@
 module UsersHelper
+
+  def show_posts?(user)
+    if user.posts.count >=1
+      return true
+    else
+      return false
+    end
+  end
+
+  def show_comments?(user)
+    if user.comments.count >=1
+      return true
+    else
+      return false
+    end
+  end
+
 end
